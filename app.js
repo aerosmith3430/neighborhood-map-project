85110d06ba5692ac3c3bee25950798bc89bee253
aerosmith3430 <aerosmith3430@gmail.com>
2017-01-02 14:00:09 -0800

    Added files and directories

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ var model = ;
 var view = ;
 
 var ViewModel = ;
+
+
+var map;
+function initMap() {
+// Constructor creates a new map - only center and zoom are required.
+map = new google.maps.Map(document.getElementById('map'), {
+	center: {lat: 45.432795, lng: -122.377449},
+	zoom: 13
+	});
+}
