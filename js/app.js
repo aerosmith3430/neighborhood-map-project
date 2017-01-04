cfbc569489af355d8483840fc1af19146418e137
aerosmith3430 <aerosmith3430@gmail.com>
2017-01-03 19:22:28 -0800

    Worked on getting map to appear

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
-var model = ;
-
-var view = ;
-
-var ViewModel = ;
+var map;
+function initMap() {
+// Constructor creates a new map - only center and zoom are required.
+map = new google.maps.Map(document.getElementById('map'), {
+	center: {lat: 45.432795, lng: -122.377449},
+	zoom: 13
+	});
+};
