d21a89cc2c9f4016ea9d32bc58894c5406edbec5
aerosmith3430 <aerosmith3430@gmail.com>
2017-01-08 11:55:09 -0800

    Added search bar

--- js/app.js
+++ js/app.js
@@ -62,7 +62,7 @@ function populateInfoWindow(marker, infowindow) {
 		infowindow.setMarker(null);
 		});
 	}
-}
+}*/
 
 // API calls to be used later
 /*var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + city + '&sort=newest&api-key=e768dfce820c43968d90887f6add8612';
@@ -99,7 +99,7 @@ function populateInfoWindow(marker, infowindow) {
     });
 
     return false;
-
+*/
 var model = [
 	{title: 'Hillsboro Hops', location: {lat: 45.554389, lng: -122.908428}},
 	{title: 'Salem-Keizer Volcanoes', location: {lat: 45.016184, lng: -122.998177}},
@@ -109,7 +109,7 @@ var model = [
 	{title: 'Spokane Indians', location: {lat: 47.662716, lng: -117.346912}},
 	{title: 'Everett Aquasox', location: {lat: 47.967024, lng: -122.201513}},
 	{title: 'Vancouver Canadians', location: {lat: 49.243227, lng: -123.105238}}
-]*/
+]
 
 /*var ViewModel = function () {
 	this.firstName = ko.observable("Bert");
@@ -123,12 +123,80 @@ var model = [
     };
 }
 
-ko.applyBindings(new ViewModel());*/
+ko.applyBindings(new ViewModel());
+*/
 
-/*var ViewModel = function () {
+var map;
+var markers = [];
+
+function initMap() {
+	// Constructor creates a new map - only center and zoom are required.
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 45.432795, lng: -122.377449},
+		zoom: 13,
+		mapTypeControl: false
+	});
+
+	var largeInfowindow = new google.maps.InfoWindow();
+	var bounds = new google.maps.LatLngBounds();
+
+    // The following group uses the location array to create an array of markers on initialize.
+	for (var i = 0; i < model.length; i++) {
+	// Get the position from the location array.
+		var position = model[i].location;
+		var title = model[i].title;
+	// Create a marker per location, and put into markers array.
+		var marker = new google.maps.Marker({
+			map: map,
+			position: position,
+			title: title,
+			animation: google.maps.Animation.DROP,
+			id: i
+		});
+		// Push the marker to our array of markers.
+		markers.push(marker);
+		// Create an onclick event to open an infowindow at each marker.
+		marker.addListener('click', function() {
+		populateInfoWindow(this, largeInfowindow);
+		});
+		bounds.extend(markers[i].position);
+		}
+		// Extend the boundaries of the map for each marker
+		map.fitBounds(bounds); 
+	};
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+// Check to make sure the infowindow is not already opened on this marker.
+	if (infowindow.marker != marker) {
+		infowindow.marker = marker;
+		infowindow.setContent('<div>' + marker.title + '</div>');
+		infowindow.open(map, marker);
+	// Make sure the marker property is cleared if the infowindow is closed.
+	infowindow.addListener('closeclick',function(){
+		infowindow.setMarker(null);
+		});
+	}
+}
+
+var View = function(data) {
+	var self = this;
+
+	self.title = ko.observable(data.title);
+}
+
+var ViewModel = function () {
     var self = this;
 
-    function SeatReservation(name, initialMeal) {
+    self.stadiumList = ko.observableArray([]);
+
+    model.forEach(function(stadium) {
+    	self.stadiumList.push( new View(stadium));
+    });     
+
+    /*function SeatReservation(name, initialMeal) {
 	    var self = this;
 	    self.name = name;
 	    self.meal = ko.observable(initialMeal);
@@ -162,7 +230,7 @@ ko.applyBindings(new ViewModel());*/
     		total += self.seats()[i].meal().price;
     	};
     	return total;
-    });
+    });*/
 }
 
-ko.applyBindings(new ViewModel());*/
+ko.applyBindings(new ViewModel());
