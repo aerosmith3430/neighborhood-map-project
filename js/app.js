f2fe30bc2d0951514fbe3e429b022745fdacbe60
aerosmith3430 <aerosmith3430@gmail.com>
2017-01-12 18:42:13 -0800

    Still working on list

--- js/app.js
+++ js/app.js
@@ -214,7 +214,31 @@ var ViewModel = function() {
     	});
     });
 
-    /*function SeatReservation(name, initialMeal) {
+    /*
+	var places = [ 
+  {"title" : "apple"},
+  {"title" : "lemon"},
+  {"title" : "orange"}
+];
+
+var viewModel = function() {
+  var self = this;
+
+  self.points = ko.observableArray(places);
+
+  self.query = ko.observable('');
+
+  self.search = ko.computed(function(){
+    return ko.utils.arrayFilter(self.points(), function(point){
+      return point.title.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
+    });
+  });
+}; 
+
+ko.applyBindings(new viewModel());
+
+
+    function SeatReservation(name, initialMeal) {
 	    var self = this;
 	    self.name = name;
 	    self.meal = ko.observable(initialMeal);
